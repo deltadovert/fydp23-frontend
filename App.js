202bfc0000966a39c0569d777199fda9196efe1a
Elijah O'Leary <elij2000@gmail.com>
2023-01-19 15:35:40 -0500

    initial commit

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Wazzzzzup</Text>
+
+      <Button onPress={() => {}} title="Brew now" />
+
       <StatusBar style="auto" />
     </View>
   );
