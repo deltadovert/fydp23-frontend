8bd9f1d2171ed70e5358faf2d0a95c471f67c3dd
Elijah O'Leary <elij2000@gmail.com>
2023-01-19 17:51:31 -0500

    added brew status

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native';
+import BrewStatus from './BrewStatus';
 import GenericScreen from './GenericScreen';
 
 const Home = ({ navigation }) => {
@@ -9,6 +10,7 @@ const Home = ({ navigation }) => {
 
   return (
     <GenericScreen title="Welcome Back to BrewX!">
+      <BrewStatus />
       <Button onPress={onScheduleNewBrew} title="Schedule a new brew" />
       <Button onPress={() => {}} title="Brew now" />
     </GenericScreen>
